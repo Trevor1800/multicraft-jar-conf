03c763ecd0461f4b827b49bac1adb449be6ed9a1
Trevor1800 <119779141+Trevor1800@users.noreply.github.com>
2022-12-04 02:03:48 -0700

    Create all.sh

---
+++ QUICK-INSTALL/scripts/all.sh
@@ -0,0 +1,293 @@
+#!/bin/bash
+# Everything config get
+echo -e "
+  __  __       _ _   _                 __ _                 _   _    ____     ____             __ _       
+ |  \/  |_   _| | |_(_) ___ _ __ __ _ / _| |_              | | / \  |  _ \   / ___|___  _ __  / _(_) __ _ 
+ | |\/| | | | | | __| |/ __|  __/ _  | |_| __|  _____   _  | |/ _ \ | |_) | | |   / _ \|  _ \| |_| |/ _  |
+ | |  | | |_| | | |_| | (__| | | (_| |  _| |_  |_____| | |_| / ___ \|  _ <  | |__| (_) | | | |  _| | (_| |
+ |_|  |_|\__,_|_|\__|_|\___|_|  \__,_|_|  \__|          \___/_/   \_\_| \_\  \____\___/|_| |_|_| |_|\__, |
+                                                                                                    |___/ "
+echo -e "\033[0;36m ## \033[0;32mLet's start getting all these things baby ! \033[37m"
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.19.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.19.2.jar.conf
+chmod 755 ./jar/vanilla-1.19.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.19.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.19.jar.conf
+chmod 755 ./jar/vanilla-1.19.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.18.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.18.2.jar.conf
+chmod 755 ./jar/vanilla-1.18.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.18.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.18.1.jar.conf
+chmod 755 ./jar/vanilla-1.18.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.18.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.18.jar.conf
+chmod 755 ./jar/vanilla-1.18.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.17.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.17.1.jar.conf
+chmod 755 ./jar/vanilla-1.17.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.17.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.17.jar.conf
+chmod 755 ./jar/vanilla-1.17.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.5.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.5.jar.conf
+chmod 755 ./jar/vanilla-1.16.5.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.4.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.4.jar.conf
+chmod 755 ./jar/vanilla-1.16.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.3.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.3.jar.conf
+chmod 755 ./jar/vanilla-1.16.3.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.2.jar.conf
+chmod 755 ./jar/vanilla-1.16.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.1.jar.conf
+chmod 755 ./jar/vanilla-1.16.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.16.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.16.jar.conf
+chmod 755 ./jar/vanilla-1.16.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.15.2.jar.conf
+chmod 755 ./jar/vanilla-1.15.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.15.1.jar.conf
+chmod 755 ./jar/vanilla-1.15.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.15.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.15.jar.conf
+chmod 755 ./jar/vanilla-1.15.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.4.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.14.4.jar.conf
+chmod 755 ./jar/vanilla-1.14.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.3.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.14.3.jar.conf
+chmod 755 ./jar/vanilla-1.14.3.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.14.2.jar.conf
+chmod 755 ./jar/vanilla-1.14.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.14.1.jar.conf
+chmod 755 ./jar/vanilla-1.14.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.14.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.14.jar.conf
+chmod 755 ./jar/vanilla-1.14.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.13.2.jar.conf
+chmod 755 ./jar/vanilla-1.13.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.13.1.jar.conf
+chmod 755 ./jar/vanilla-1.13.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.13.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.13.jar.conf
+chmod 755 ./jar/vanilla-1.13.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.12.2.jar.conf
+chmod 755 ./jar/vanilla-1.12.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.12.1.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.12.1.jar.conf
+chmod 755 ./jar/vanilla-1.12.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.11.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.11.2.jar.conf
+chmod 755 ./jar/vanilla-1.11.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.10.2.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.10.2.jar.conf
+chmod 755 ./jar/vanilla-1.10.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.9.4.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.9.4.jar.conf
+chmod 755 ./jar/vanilla-1.9.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/vanilla/vanilla-1.8.9.jar.conf
+chown minecraft:minecraft ./jar/vanilla-1.8.9.jar.conf
+chmod 755 ./jar/vanilla-1.8.9.jar.conf
+
+echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
+echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mVANILLA \033[0;32mfiles ! Great ! \033[37m"
+echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.19.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.19.2.jar.conf
+chmod 755 ./jar/spigot-1.19.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.18.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.18.1.jar.conf
+chmod 755 ./jar/spigot-1.18.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.18.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.18.jar.conf
+chmod 755 ./jar/spigot-1.18.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.17.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.17.1.jar.conf
+chmod 755 ./jar/spigot-1.17.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.17.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.17.jar.conf
+chmod 755 ./jar/spigot-1.17.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.16.5.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.16.5.jar.conf
+chmod 755 ./jar/spigot-1.16.5.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.16.4.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.16.4.jar.conf
+chmod 755 ./jar/spigot-1.16.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.16.3.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.16.3.jar.conf
+chmod 755 ./jar/spigot-1.16.3.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.16.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.16.2.jar.conf
+chmod 755 ./jar/spigot-1.16.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.16.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.16.1.jar.conf
+chmod 755 ./jar/spigot-1.16.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.15.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.15.2.jar.conf
+chmod 755 ./jar/spigot-1.15.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.15.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.15.1.jar.conf
+chmod 755 ./jar/spigot-1.15.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.15.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.15.jar.conf
+chmod 755 ./jar/spigot-1.15.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.14.4.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.14.4.jar.conf
+chmod 755 ./jar/spigot-1.14.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.14.3.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.14.3.jar.conf
+chmod 755 ./jar/spigot-1.14.3.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.14.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.14.2.jar.conf
+chmod 755 ./jar/spigot-1.14.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.14.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.14.1.jar.conf
+chmod 755 ./jar/spigot-1.14.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.14.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.14.jar.conf
+chmod 755 ./jar/spigot-1.14.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.13.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.13.2.jar.conf
+chmod 755 ./jar/spigot-1.13.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.12.2.jar.conf
+chmod 755 ./jar/spigot-1.12.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.12.1.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.12.1.jar.conf
+chmod 755 ./jar/spigot-1.12.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.11.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.11.2.jar.conf
+chmod 755 ./jar/spigot-1.11.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.10.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.10.2.jar.conf
+chmod 755 ./jar/spigot-1.10.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.9.4.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.9.4.jar.conf
+chmod 755 ./jar/spigot-1.9.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/spigot/spigot-1.8.8.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.8.8.jar.conf
+chmod 755 ./jar/spigot-1.8.8.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.17.1.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.17.1.jar.conf
+chmod 755 ./jar/paperspigot-1.17.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.5.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.16.5.jar.conf
+chmod 755 ./jar/paperspigot-1.16.5.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.4.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.16.4.jar.conf
+chmod 755 ./jar/paperspigot-1.16.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.16.3.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.16.3.jar.conf
+chmod 755 ./jar/paperspigot-1.16.3.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.15.2.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.15.2.jar.conf
+chmod 755 ./jar/paperspigot-1.15.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.15.1.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.15.1.jar.conf
+chmod 755 ./jar/paperspigot-1.15.1.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.14.4.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.14.4.jar.conf
+chmod 755 ./jar/paperspigot-1.14.4.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.13.2.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.13.2.jar.conf
+chmod 755 ./jar/paperspigot-1.13.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.12.2.jar.conf
+chmod 755 ./jar/paperspigot-1.12.2.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/Trevor1800/MultiCraft-JAR-Conf/master/minecraft/paperspigot/paperspigot-1.8.8.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.8.8.jar.conf
+chmod 755 ./jar/paperspigot-1.8.8.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/custom/java8.jar.conf
+chown minecraft:minecraft ./jar/java8.jar.conf
+chmod 755 ./jar/java8.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/custom/java11.jar.conf
+chown minecraft:minecraft ./jar/java11.jar.conf
+chmod 755 ./jar/java11.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/custom/java17.jar.conf
+chown minecraft:minecraft ./jar/java17.jar.conf
+chmod 755 ./jar/java17.jar.conf
+
+wget -N -P ./jar http://raw.githubusercontent.com/ValentinTh/MultiCraft-JAR-Conf/master/minecraft/custom/waterfall1.19.jar.conf
+chown minecraft:minecraft ./jar/waterfall1.19.jar.conf
+chmod 755 ./jar/waterfall1.19.jar.conf
+
+
+rm -r all.sh
+exit
